82904d562e4b050f5f826517d38e1c378e470ae8
kimhanguk <khanguk@naver.com>
2016-10-27 20:02:05 +0900

    DB insert

--- cloudPortal/models.py
+++ cloudPortal/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
 # Create your models here.
+
+
+class GlobalConfig(models.Model):
+    vc_host = models.CharField(max_length=50, blank=True, null=True)
+    vc_user = models.CharField(max_length=30, blank=True, null=True)
+    vc_pass = models.CharField(max_length=30, blank=True, null=True)
+    vc_port = models.CharField(max_length=10, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.vc_host
+
+
+class BiHost(models.Model):
+    datacenter = models.CharField(max_length=100, blank=True, null=True)
+    cluster = models.CharField(max_length=100, blank=True, null=True)
+    host = models.CharField(max_length=100, blank=True, null=True)
+    os = models.CharField(max_length=50, blank=True, null=True)
+    version = models.CharField(max_length=20, blank=True, null=True)
+    ip = models.CharField(max_length=20, blank=True, null=True)
+    status = models.CharField(max_length=20, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.host
+
+    class Meta:
+        ordering = ('datacenter', 'cluster', 'host',)
+
+
+class BiVnic(models.Model):
+    device = models.CharField(max_length=100, blank=True, null=True)
+    ipAddress = models.CharField(max_length=20, blank=True, null=True)
+    host = models.ManyToManyField(BiHost)
+
+    def __unicode__(self):
+        return self.device
+
+    class Meta:
+        ordering = ('ipAddress',)
+
+
+class BiVswitch(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    host = models.ManyToManyField(BiHost)
+
+    def __unicode__(self):
+        return self.name
+
+
+class BiVolume(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    host = models.ManyToManyField(BiHost)
+
+    def __unicode__(self):
+        return self.name
+
+
+class BiVirtualMachine(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    ipAddress = models.CharField(max_length=20, blank=True, null=True)
+    macAddress = models.CharField(max_length=20, blank=True, null=True)
+    cpuUsage = models.CharField(max_length=5, blank=True, null=True)
+    memUsage = models.CharField(max_length=5, blank=True, null=True)
+    netUsage = models.CharField(max_length=5, blank=True, null=True)
+    stgUsage = models.CharField(max_length=30, blank=True, null=True)
+    status = models.CharField(max_length=20, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
--- cloudPortal/views.py
+++ cloudPortal/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
+import atexit
+
+from pyVim import connect
+from pyVmomi import vmodl
+from pyVmomi import vim
+
+from models import GlobalConfig, BiHost, BiVnic, BiVolume, BiVswitch, BiVirtualMachine
+
+import tools.cli as cli
+
+import ssl
 
 # Create your views here.
 
@@ -8,11 +21,144 @@ def dashboard(request):
 
 
 def hosts(request):
-    return render(request, 'hostList.html', {})
+    try:
+
+        config = GlobalConfig.objects.all()
+        context = ssl._create_unverified_context()
+        service_instance = connect.SmartConnect(host=config[0].vc_host,
+                                                user=config[0].vc_user,
+                                                pwd=config[0].vc_pass,
+                                                port=int(config[0].vc_port), sslContext=context)
+
+        atexit.register(connect.Disconnect, service_instance)
+
+        content = service_instance.RetrieveContent()
+
+        container = content.rootFolder  # starting point to look into
+        viewType = [vim.HostSystem]  # object types to look for
+        recursive = True  # whether we should look into it recursively
+        containerView = content.viewManager.CreateContainerView(
+            container, viewType, recursive)
+
+        children = containerView.view
+        results = []
+        for child in children:
+            print("summary :", child.summary)
+            h = BiHost()
+            h.host = child.summary.host
+            h.os = child.summary.config.product.osType
+            h.version = child.summary.config.product.version
+            h.ip = child.summary.managementServerIp
+            h.status = child.summary.overallStatus
+            h.save()
+
+            for vnic in child.summary.host.configManager.networkSystem.networkConfig.vnic:
+                n = BiVnic()
+                n.device = vnic.device
+                n.ipAddress = vnic.spec.ip.ipAddress
+                n.host.add(h)
+                n.save()
+
+            for vswitch in child.summary.host.configManager.networkSystem.networkConfig.vswitch:
+                s = BiVswitch()
+                s.name = vswitch.name
+                s.host.add(h)
+                s.save()
+
+            for vol in child.summary.host.configManager.storageSystem.fileSystemVolumeInfo.mountInfo:
+                v = BiVolume()
+                v.name = vol.volume.name
+                v.host.add(h)
+                v.save()
+        # print_vm_info(child)
+        #     jsonObject = {}
+        #     jsonObject['Name'] = child.summary.config.name
+        #     jsonObject['IP'] = child.summary.guest.ipAddress
+        #     results.append(jsonObject)
+
+    except vmodl.MethodFault as error:
+        print("Caught vmodl fault : " + error.msg)
+
+    if request.is_ajax():
+        return HttpResponse(json.dumps(results), 'application/json')
+    return render(request, 'hostList.html', {'list': children})
 
 
 def vms(request):
-    return render(request, 'vmList.html', {})
+
+    try:
+
+        config = GlobalConfig.objects.all()
+        context = ssl._create_unverified_context()
+        service_instance = connect.SmartConnect(host=config[0].vc_host,
+                                                user=config[0].vc_user,
+                                                pwd=config[0].vc_pass,
+                                                port=int(config[0].vc_port), sslContext=context)
+
+        atexit.register(connect.Disconnect, service_instance)
+
+        content = service_instance.RetrieveContent()
+
+        container = content.rootFolder  # starting point to look into
+        viewType = [vim.VirtualMachine]  # object types to look for
+        recursive = True  # whether we should look into it recursively
+        containerView = content.viewManager.CreateContainerView(
+            container, viewType, recursive)
+
+        children = containerView.view
+        results = []
+        for child in children:
+            #print_vm_info(child)
+            jsonObject = {}
+            jsonObject['Name'] = child.summary.config.name
+            jsonObject['IP'] = child.summary.guest.ipAddress
+            results.append(jsonObject)
+
+            vm = BiVirtualMachine()
+            vm.name = child.summary.config.name
+            vm.ipAddress = child.summary.guest.ipAddress
+            vm.macAddress = None
+            vm.cpuUsage = child.summary.quickStats.overallCpuUsage
+            vm.memUsage = child.quickStats.guestMemoryUsage
+            vm.netUsage = None
+            vm.stgUsage = child.summary.storage.committed
+            vm.status = None
+            vm.save()
+
+    except vmodl.MethodFault as error:
+        print("Caught vmodl fault : " + error.msg)
+
+    if request.is_ajax():
+        return HttpResponse(json.dumps(results), 'application/json')
+    return render(request, 'vmList.html', {'list': children})  # {'list': children}
+
+
+def vmsAjax(request):
+
+    try:
+        context = ssl._create_unverified_context()
+
+        service_instance = connect.SmartConnect(host="198.18.133.30",
+                                                user="root",
+                                                pwd="C1sco12345!",
+                                                port=int("443"), sslContext = context)
+
+        atexit.register(connect.Disconnect, service_instance)
+
+        content = service_instance.RetrieveContent()
+
+        container = content.rootFolder  # starting point to look into
+        viewType = [vim.VirtualMachine]  # object types to look for
+        recursive = True  # whether we should look into it recursively
+        containerView = content.viewManager.CreateContainerView(
+            container, viewType, recursive)
+
+        children = containerView.view
+
+    except vmodl.MethodFault as error:
+        print("Caught vmodl fault : " + error.msg)
+
+    return HttpResponse( json.dumps({'list': children}), 'application/json')
 
 
 def vnets(request):
@@ -20,7 +166,41 @@ def vnets(request):
 
 
 def volumes(request):
-    return render(request, 'volumeList.html', {})
+    try:
+
+        config = GlobalConfig.objects.all()
+        context = ssl._create_unverified_context()
+        service_instance = connect.SmartConnect(host=config[0].vc_host,
+                                                user=config[0].vc_user,
+                                                pwd=config[0].vc_pass,
+                                                port=int(config[0].vc_port), sslContext=context)
+
+        atexit.register(connect.Disconnect, service_instance)
+
+        content = service_instance.RetrieveContent()
+
+        container = content.rootFolder  # starting point to look into
+        viewType = [vim.HostSystem]  # object types to look for
+        recursive = True  # whether we should look into it recursively
+        containerView = content.viewManager.CreateContainerView(
+            container, viewType, recursive)
+
+        children = containerView.view
+        results = []
+        for child in children:
+            print("mountInfo :", child.configManager.storageSystem.fileSystemVolumeInfo.mountInfo)
+            # print_vm_info(child)
+        #     jsonObject = {}
+        #     jsonObject['Name'] = child.summary.config.name
+        #     jsonObject['IP'] = child.summary.guest.ipAddress
+        #     results.append(jsonObject)
+
+    except vmodl.MethodFault as error:
+        print("Caught vmodl fault : " + error.msg)
+
+    if request.is_ajax():
+        return HttpResponse(json.dumps(results), 'application/json')
+    return render(request, 'volumeList.html', {'list': children})
 
 
 def disks(request):
@@ -35,3 +215,33 @@ def users(request):
     return render(request, 'userList.html', {})
 
 
+def print_vm_info(virtual_machine):
+    """
+    Print information for a particular virtual machine or recurse into a
+    folder with depth protection
+    """
+    summary = virtual_machine.summary
+    print("Name       : ", summary.config.name)
+    print("Template   : ", summary.config.template)
+    print("Path       : ", summary.config.vmPathName)
+    print("Guest      : ", summary.config.guestFullName)
+    print("Instance UUID : ", summary.config.instanceUuid)
+    print("Bios UUID     : ", summary.config.uuid)
+    annotation = summary.config.annotation
+    if annotation:
+        print("Annotation : ", annotation)
+    print("State      : ", summary.runtime.powerState)
+    if summary.guest is not None:
+        ip_address = summary.guest.ipAddress
+        tools_version = summary.guest.toolsStatus
+        if tools_version is not None:
+            print("VMware-tools: ", tools_version)
+        else:
+            print("Vmware-tools: None")
+        if ip_address:
+            print("IP         : ", ip_address)
+        else:
+            print("IP         : None")
+    if summary.runtime.question is not None:
+        print("Question  : ", summary.runtime.question.text)
+    print("")
