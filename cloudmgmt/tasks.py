0e7168860656840e861f59a231793cc621d776b3
Hyung-Soo Kim <javaos74@gmail.com>
2017-02-01 13:18:05 +0900

    update celery configuration remove from ux, only cloudmgmt has celery config

--- cloudmgmt/tasks.py
+++ cloudmgmt/tasks.py
@@ -9,39 +9,3 @@ def update_all():
     print("update_all Start")
     reload_data(request=None)
     print("update_all End")
-
-
-@app.task
-def update_dcs():
-    print("update_dcs")
-    pass
-
-
-@app.task
-def update_clusters():
-    print("update_clusters")
-    pass
-
-
-@app.task
-def update_hosts():
-    print("update_hosts")
-    pass
-
-
-@app.task
-def update_vms():
-    print("update_vms")
-    pass
-
-
-@app.task
-def update_vswitchs():
-    print("update_vswitchs")
-    pass
-
-
-@app.task
-def update_portgroups():
-    print("update_portgroups")
-    pass
